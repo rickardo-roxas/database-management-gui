54880ee99cb1bc19619c6509cd1d772f3ae680ac
Hannah Ragudos <hannahragudos@gmail.com>
2023-03-14 21:01:51 +0800

    Updated readFraction method with exception handling

--- DeTorres_Delizo_Lacanilao_Limboc_Ragudos_Roxas_9301_08/src/edu/slu/prog2/midgroup08/FractionTester.java
+++ DeTorres_Delizo_Lacanilao_Limboc_Ragudos_Roxas_9301_08/src/edu/slu/prog2/midgroup08/FractionTester.java
@@ -54,7 +54,74 @@ public class FractionTester {
      * @return Mixed Fraction with user given whole number, numerator, and denominator.
      */
     public static MixedFraction readFraction() {
-        MixedFraction mixedFraction = new MixedFraction();
-        return mixedFraction;
+        int wholeNumber;
+        int numerator;
+        int denominator;
+        System.out.println("Enter a fraction (in the format w n/d): ");
+        inputString = keyboard.nextLine();
+
+        try {
+            String[] parts = inputString.split(" ");
+            wholeNumber = Integer.parseInt(parts[0]);
+            String[] parts2 = parts[1].split("/");
+            numerator = Integer.parseInt(parts2[0]);
+            denominator = Integer.parseInt(parts2[1]);
+            return new MixedFraction(wholeNumber, numerator, denominator);
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            System.out.println("Invalid input. Please try again.");
+            return readFraction();
+        }
+    }
+
+    /**
+     * Converts a mixed fraction to an improper fraction.
+     * @param mixedFraction the mixed fraction to convert
+     * @return the improper fraction as a string
+     * @throws IllegalArgumentException if the input is not in the correct format
+     */
+    public static String convertToImproper(String mixedFraction) throws IllegalArgumentException {
+        String[] parts = mixedFraction.split("/");
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Invalid fraction: " + mixedFraction);
+        }
+        try {
+            int wholeNumber = Integer.parseInt(parts[0]);
+            int numerator = Integer.parseInt(parts[1]);
+            int denominator = 1;
+            if (wholeNumber < 0) {
+                numerator *= -1;
+            }
+            numerator += (Math.abs(wholeNumber) * denominator);
+            return numerator + "/" + denominator;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid fraction: " + mixedFraction);
+        }
+    }
+
+    /**
+
+     Converts an improper fraction to a mixed number.
+     @param improperFraction the improper fraction to convert
+     @return the mixed number as a string
+     @throws IllegalArgumentException if the input is not in the correct format
+     */
+    public static String convertToMixedNumbers(String improperFraction) throws IllegalArgumentException {
+        String[] parts = improperFraction.split("/");
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Invalid fraction: " + improperFraction);
+        }
+        try {
+            int numerator = Integer.parseInt(parts[0]);
+            int denominator = Integer.parseInt(parts[1]);
+            int wholeNumber = numerator / denominator;
+            numerator = Math.abs(numerator % denominator);
+            if (wholeNumber == 0) {
+                return numerator + "/" + denominator;
+            } else {
+                return wholeNumber + " " + numerator + "/" + denominator;
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid fraction: " + improperFraction);
+        }
     } // end of readFraction method
 } // end of class FractionTester
