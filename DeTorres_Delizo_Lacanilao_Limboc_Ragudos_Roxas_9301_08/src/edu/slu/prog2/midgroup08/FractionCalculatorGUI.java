0c42da5168804b9371d9200cbb555c2a8f242e4e
Hannah Ragudos <hannahragudos@gmail.com>
2023-03-19 10:48:12 +0800

    Added documentation on FractionCalculatorGUI

--- DeTorres_Delizo_Lacanilao_Limboc_Ragudos_Roxas_9301_08/src/edu/slu/prog2/midgroup08/FractionCalculatorGUI.java
+++ DeTorres_Delizo_Lacanilao_Limboc_Ragudos_Roxas_9301_08/src/edu/slu/prog2/midgroup08/FractionCalculatorGUI.java
@@ -1,3 +1,23 @@
+/**
+ * Group 8
+ * Class Code and Course Number: 9301 - CS 122
+ * Schedule: TF 9:00 - 10:30 AM
+ * <p>
+ *     LACANILAO, Marvin Patrick D. - 2230153
+ *     DE TORRES, John Rey I. - 2234944
+ *     DELIZO, John Marco - 2234758
+ *     LIMBOC, Trixie Mae W. - 2233674
+ *     RAGUDOS, Hannah T.- 2233361
+ *     ROXAS, Johan Rickardo A. - 2233375
+ * </p>
+ * The FractionCalculatorGUI
+ * <p>
+ *     This class represents the graphical user interface for a
+ *     simple fraction calculator. It extends the JFrame class and
+ *     implements the ActionListener interface for event handling.
+ * </p>
+ */
+
 package edu.slu.prog2.midgroup08;
 import javax.swing.*;
 import java.awt.*;
