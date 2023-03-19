ea00f3a526df80d96428070c10404a99acd6fd0a
Hannah Ragudos <hannahragudos@gmail.com>
2023-03-19 21:21:11 +0800

    Added a MixedFractionCalculator GUI class

--- DeTorres_Delizo_Lacanilao_Limboc_Ragudos_Roxas_9301_08/src/edu/slu/prog2/midgroup08/FractionCalculatorOptionalGUI.java
+++ DeTorres_Delizo_Lacanilao_Limboc_Ragudos_Roxas_9301_08/src/edu/slu/prog2/midgroup08/FractionCalculatorOptionalGUI.java
@@ -1,46 +1,55 @@
+/**
+ * Group 8
+ * Class Code and Course Number: 9301 - CS 122
+ * Schedule: TF 9:00 - 10:30 AM
+ * <p>
+ *     LACANILAO, Marvin Patrick D. - 2230153
+ *     DE TORRES, John Rey I. - 2234944
+ *     DELIZO, John Marco - 2234758
+ *     LIMBOC, Trixie Mae W. - 2233674
+ *     RAGUDOS, Hannah T.- 2233361
+ *     ROXAS, Johan Rickardo A. - 2233375
+ * </p>
+ * The MixedFractionCalculatorGUI
+ * <p>
+ *     This class represents the graphical user interface for a
+ *     mixed fraction calculator. It extends the JFrame class and
+ *     implements the ActionListener interface for event handling.
+ * </p>
+ */
+
 package edu.slu.prog2.midgroup08;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import edu.slu.prog2.pregroup08.Fraction;
 
+
 public class FractionCalculatorOptionalGUI extends JPanel {
-    private final JLabel fracResult;
-    private JButton addB;
-    private JButton subtractB;
-    private JButton multiplyB;
-    private JButton divideB;
-    private JLabel Fraction1;
-    private JLabel Fraction2;
-    private JTextField firstNumerator;
-    private JTextField secondNumerator;
-    private JLabel separator1;
-    private JLabel separator2;
-    private JTextField firstDenominator;
-    private JTextField secondDenominator;
-    private JTextField result;
-    private JButton clearB;
-    private JButton exitB;
+    private final JTextField firstNumerator;
+    private final JTextField secondNumerator;
+    private final JTextField firstDenominator;
+    private final JTextField secondDenominator;
+    private final JTextField result;
 
     public FractionCalculatorOptionalGUI() {
         //construct components
-        addB = new JButton("Add");
-        subtractB = new JButton("Subtract");
-        multiplyB = new JButton("Multiply");
-        divideB = new JButton("Divide");
-        fracResult = new JLabel ("Result:");
-        Fraction1 = new JLabel("Enter the First Fraction:");
-        Fraction2 = new JLabel("Enter the Second Fraction:");
+        JButton addB = new JButton("Add");
+        JButton subtractB = new JButton("Subtract");
+        JButton multiplyB = new JButton("Multiply");
+        JButton divideB = new JButton("Divide");
+        JLabel fracResult = new JLabel("Result:");
+        JLabel fraction1 = new JLabel("Enter the First Fraction:");
+        JLabel fraction2 = new JLabel("Enter the Second Fraction:");
         firstNumerator = new JTextField(5);
         secondNumerator = new JTextField(5);
-        separator1 = new JLabel("/");
-        separator2 = new JLabel("/");
+        JLabel separator1 = new JLabel("/");
+        JLabel separator2 = new JLabel("/");
         firstDenominator = new JTextField(5);
         secondDenominator = new JTextField(5);
         result = new JTextField(5);
-        clearB = new JButton("Clear");
-        exitB = new JButton("Exit");
+        JButton clearB = new JButton("Clear");
+        JButton exitB = new JButton("Exit");
 
         //adjust size and set layout
         setPreferredSize(new Dimension(424, 272));
@@ -52,8 +61,8 @@ public class FractionCalculatorOptionalGUI extends JPanel {
         add(multiplyB);
         add(divideB);
         add(fracResult);
-        add(Fraction1);
-        add(Fraction2);
+        add(fraction1);
+        add(fraction2);
         add(firstNumerator);
         add(secondNumerator);
         add(separator1);
@@ -64,14 +73,14 @@ public class FractionCalculatorOptionalGUI extends JPanel {
         add(clearB);
         add(exitB);
 
-        //set component bounds (only needed by Absolute Positioning)
+        //set component bounds
         addB.setBounds(15, 95, 90, 60);
         subtractB.setBounds(115, 95, 90, 60);
         multiplyB.setBounds(215, 95, 90, 60);
         divideB.setBounds(315, 95, 95, 60);
         fracResult.setBounds(15, 180, 100, 25);
-        Fraction1.setBounds(15, 15, 150, 25);
-        Fraction2.setBounds(15, 55, 160, 25);
+        fraction1.setBounds(15, 15, 150, 25);
+        fraction2.setBounds(15, 55, 160, 25);
         firstNumerator.setBounds(175, 15, 100, 25);
         secondNumerator.setBounds(175, 55, 100, 25);
         separator1.setBounds(290, 15, 20, 25);
@@ -83,77 +92,65 @@ public class FractionCalculatorOptionalGUI extends JPanel {
         exitB.setBounds(310, 230, 100, 25);
 
         //add action listeners
-        addB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int num1 = Integer.parseInt(firstNumerator.getText());
-                int den1 = Integer.parseInt(firstDenominator.getText());
-                int num2 = Integer.parseInt(secondNumerator.getText());
-                int den2 = Integer.parseInt(secondDenominator.getText());
-                Fraction f1 = new Fraction(num1, den1);
-                Fraction f2 = new Fraction(num2, den2);
-                //perform addition operation
-                Fraction resultFraction = f1.add(f2);
-                result.setText(resultFraction.toString());
-            }
+        addB.addActionListener(e -> {
+            int num1 = Integer.parseInt(firstNumerator.getText());
+            int den1 = Integer.parseInt(firstDenominator.getText());
+            int num2 = Integer.parseInt(secondNumerator.getText());
+            int den2 = Integer.parseInt(secondDenominator.getText());
+            Fraction f1 = new Fraction(num1, den1);
+            Fraction f2 = new Fraction(num2, den2);
+            //perform addition operation
+            Fraction resultFraction = f1.add(f2);
+            result.setText(resultFraction.toString());
         });
-        subtractB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int num1 = Integer.parseInt(firstNumerator.getText());
-                int den1 = Integer.parseInt(firstDenominator.getText());
-                int num2 = Integer.parseInt(secondNumerator.getText());
-                int den2 = Integer.parseInt(secondDenominator.getText());
-                Fraction f1 = new Fraction(num1, den1);
-                Fraction f2 = new Fraction(num2, den2);
-                //perform subtraction operation
-                Fraction resultFraction = f1.subtract(f2);
-                result.setText(resultFraction.toString());
-            }
+        subtractB.addActionListener(e -> {
+            int num1 = Integer.parseInt(firstNumerator.getText());
+            int den1 = Integer.parseInt(firstDenominator.getText());
+            int num2 = Integer.parseInt(secondNumerator.getText());
+            int den2 = Integer.parseInt(secondDenominator.getText());
+            Fraction f1 = new Fraction(num1, den1);
+            Fraction f2 = new Fraction(num2, den2);
+            //perform subtraction operation
+            Fraction resultFraction = f1.subtract(f2);
+            result.setText(resultFraction.toString());
         });
 
-        multiplyB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int num1 = Integer.parseInt(firstNumerator.getText());
-                int den1 = Integer.parseInt(firstDenominator.getText());
-                int num2 = Integer.parseInt(secondNumerator.getText());
-                int den2 = Integer.parseInt(secondDenominator.getText());
-                Fraction f1 = new Fraction(num1, den1);
-                Fraction f2 = new Fraction(num2, den2);
-                //perform multiplication operation
-                Fraction resultFraction = f1.multiplyBy(f2);
-                result.setText(resultFraction.toString());
-            }
+        multiplyB.addActionListener(e -> {
+            int num1 = Integer.parseInt(firstNumerator.getText());
+            int den1 = Integer.parseInt(firstDenominator.getText());
+            int num2 = Integer.parseInt(secondNumerator.getText());
+            int den2 = Integer.parseInt(secondDenominator.getText());
+            Fraction f1 = new Fraction(num1, den1);
+            Fraction f2 = new Fraction(num2, den2);
+            //perform multiplication operation
+            Fraction resultFraction = f1.multiplyBy(f2);
+            result.setText(resultFraction.toString());
         });
 
-        divideB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int num1 = Integer.parseInt(firstNumerator.getText());
-                int den1 = Integer.parseInt(firstDenominator.getText());
-                int num2 = Integer.parseInt(secondNumerator.getText());
-                int den2 = Integer.parseInt(secondDenominator.getText());
-                Fraction f1 = new Fraction(num1, den1);
-                Fraction f2 = new Fraction(num2, den2);
-                //perform division operation
-                Fraction resultFraction = f1.divideBy(f2);
-                result.setText(resultFraction.toString());
-            }
+        divideB.addActionListener(e -> {
+            int num1 = Integer.parseInt(firstNumerator.getText());
+            int den1 = Integer.parseInt(firstDenominator.getText());
+            int num2 = Integer.parseInt(secondNumerator.getText());
+            int den2 = Integer.parseInt(secondDenominator.getText());
+            Fraction f1 = new Fraction(num1, den1);
+            Fraction f2 = new Fraction(num2, den2);
+            //perform division operation
+            Fraction resultFraction = f1.divideBy(f2);
+            result.setText(resultFraction.toString());
         });
 
-        clearB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                //clear all text fields
-                firstNumerator.setText("");
-                secondNumerator.setText("");
-                firstDenominator.setText("");
-                secondDenominator.setText("");
-                result.setText("");
-            }
+        clearB.addActionListener(e -> {
+            //clear all text fields
+            firstNumerator.setText("");
+            secondNumerator.setText("");
+            firstDenominator.setText("");
+            secondDenominator.setText("");
+            result.setText("");
         });
 
-        exitB.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                //exit the program
-                System.exit(0);
-            }
+        exitB.addActionListener(e -> {
+            //exit the program
+            System.exit(0);
         });
     }
     public static void main(String[] args) {
