979e3a6fbb16cce744e9703d63f297eb789cb8a2
Hannah Ragudos <hannahragudos@gmail.com>
2023-04-19 16:34:20 +0800

    Created mutator methods and added documentation on Student Class

--- FinalProject1/src/finals/prog2/samcis/slu/Student.java
+++ FinalProject1/src/finals/prog2/samcis/slu/Student.java
@@ -18,37 +18,37 @@ package finals.prog2.samcis.slu;
  Class representing a student.
  */
 public class Student {
-    private String lastName;
-    private String firstName;
-    private int idNumber;
-    private int age;
-    private char gender;
-    private String courseProgram;
-    private byte yearLevel;
+    private String lastName; // The last name of the student
+    private String firstName; // The first name of the student
+    private int idNumber; // The ID number of the student
+    private int age; // The age of the student
+    private char gender; // The gender of the student (M or F)
+    private String courseProgram; // The course program of the student
+    private byte yearLevel; // The year level of the student
 
     /**
      * Constructs an object of Student with default values.
      */
     public Student() {
-        lastName = "Doe";
-        firstName = "John";
-        idNumber = 0;
-        age = 18;
+        lastName = "Pascual";
+        firstName = "Scheherazade";
+        idNumber = 2233444;
+        age = 19;
         gender = 'M';
         courseProgram = "BS Computer Science";
         yearLevel = 1;
-    }
+    } // end of Student default constructor
 
     /**
      * Constructs an object of Student with user-defined values.
      *
-     * @param lastName      the last name of the student
-     * @param firstName     the first name of the student
-     * @param idNumber      the ID number of the student
-     * @param age           the age of the student
-     * @param gender        the gender of the student (M or F)
-     * @param courseProgram the course program of the student
-     * @param yearLevel     the year level of the student
+     * @param lastName      The last name of the student
+     * @param firstName     The first name of the student
+     * @param idNumber      The ID number of the student
+     * @param age           The age of the student
+     * @param gender        The gender of the student (M or F)
+     * @param courseProgram The course program of the student
+     * @param yearLevel     The year level of the student
      */
     public Student(String lastName, String firstName, int idNumber, int age, char gender, String courseProgram, byte yearLevel) {
         this.lastName = lastName;
@@ -58,5 +58,62 @@ public class Student {
         this.gender = gender;
         this.courseProgram = courseProgram;
         this.yearLevel = yearLevel;
-    }
-}
+    } // end of Student constructor
+
+    /**
+     * Mutator method for the last name attribute.
+     * @param lastName The new last name of the student
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    } // end of setLastName mutator method
+
+    /**
+     * Mutator method for the first name attribute.
+     * @param firstName The new first name of the student
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    } // end of setFirstName mutator method
+
+    /**
+     * Mutator method for the ID number attribute.
+     * @param idNumber The new ID number attribute of the student
+     */
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    } // end of setIdNumber mutator method
+
+    /**
+     * Mutator method for the age attribute.
+     * @param age The new age of the student
+     */
+    public void setAge(int age) {
+        this.age = age;
+    } // end of setAge mutator method
+
+    /**
+     * Mutator method for the gender attribute.
+     * @param gender The new gender of the student (M or F)
+     */
+    public void setGender(char gender) {
+        this.gender = gender;
+    } // end of setGender mutator method
+
+    /**
+     * Mutator method for the course program attribute.
+     * @param courseProgram The new course program of the student
+     */
+    public void setCourseProgram(String courseProgram) {
+        this.courseProgram = courseProgram;
+    } // end of setCourseProgram mutator method
+
+    /**
+     * Mutator method for the year level attribute.
+     * @param yearLevel The new year level of the student
+     */
+    public void setYearLevel(byte yearLevel) {
+        this.yearLevel = yearLevel;
+    } // end of setYearLevel mutator method
+} // end of Student Class
+
