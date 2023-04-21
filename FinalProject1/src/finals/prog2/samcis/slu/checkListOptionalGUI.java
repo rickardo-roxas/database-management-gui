d6ee1c1085e089ca9895a8d101434ad13de5d021
Trixie <2233674@slu.edu.ph>
2023-04-22 01:14:00 +0800

    Added a gui sample for the student information / reference class

---
+++ FinalProject1/src/finals/prog2/samcis/slu/checkListOptionalGUI.java
@@ -0,0 +1,72 @@
+/**
+ * Group 8
+ * Class Code and Course Number: 9301 - CS 122
+ * Schedule: TF 9:00 - 10:30 AM
+ * <p>
+ *     LACANILAO, Marvin Patrick D. - 2230153
+ *     DE TORRES, John Rey I. - 2234944
+ *     DELIZO, John Marco - 2234758
+ *     LIMBOC, Trixie Mae W. - 2233674
+ *     RAGUDOS, Hannah T.- 2233361
+ *     ROXAS, Johan Rickardo A. - 2233375
+ * </p>
+ *
+ * ---ONGOING---
+ */
+package finals.prog2.samcis.slu;
+import javax.swing.*;
+
+public class checkListOptionalGUI {
+
+    private JPanel introPanel;
+    private JTextPane TextPane;
+    private JLabel idNumber, lastName, firstName, age, gender, courseProgram, yearLevel;
+    private JTextField idNumberTF, lastNameTF, firstNameTF, ageTF, courseProgramTF, yearLevelTF;
+    private JCheckBox maleCheckBox;
+    private JCheckBox femaleCheckBox;
+    private JButton nextButton;
+    private JButton exitButton;
+
+    private JFrame menuCheckList;
+
+    public static void main(String[] args) {
+
+      }
+
+
+    public void CheckListManagementOptionalGUI() {
+    /**
+        checkList.setTitle("Curriculum CheckList");
+        checkList.setSize(450, 600);
+        checkList.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        checkList.setResizable(false);
+
+
+        TextPane.add(introPanel);
+        idNumber.add(introPanel);
+        lastName.add(introPanel);
+        firstName.add(introPanel);
+        age.add(introPanel);
+        gender.add(introPanel);
+        courseProgram.add(introPanel);
+        yearLevel.add(introPanel);
+        idNumberTF.add(introPanel);
+        lastNameTF.add(introPanel);
+        firstNameTF.add(introPanel);
+        ageTF.add(introPanel);
+        courseProgramTF.add(introPanel);
+        yearLevelTF.add(introPanel);
+        maleCheckBox.add(introPanel);
+        femaleCheckBox.add(introPanel);
+        nextButton.add(introPanel);
+        exitButton.add(introPanel);
+        introPanel.add(introPanel);
+      }
+
+       private void menuCheckList () throws Exception{
+        menuCheckList = new JFrame("MENU");
+       }
+    */
+ }
+}
+
