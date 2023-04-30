1a62e3f0623018d1cdf83ea126627607d0b70514
Johan Rickardo A. Roxas <2233375@slu.edu.ph>
2023-04-30 21:31:05 +0800

    Initial commit for the MyProgram source code.

---
+++ FinalProject2/src/prog2/finalgroup/MyProgram.java
@@ -0,0 +1,25 @@
+/**
+ * Group 8
+ * Class Code and Course Number: 9301 - CS 122
+ * Schedule: TF 9:00 - 10:30 AM
+ * <p>
+ *     LACANILAO, Marvin Patrick D. - 2230153
+ *     DE TORRES, John Rey I. - 2234944
+ *     DELIZO, John Marco - 2234758
+ *     LIMBOC, Trixie Mae W. - 2233674
+ *     RAGUDOS, Hannah T.- 2233361
+ *     ROXAS, Johan Rickardo A. - 2233375
+ * </p>
+ */
+package prog2.finalgroup;
+
+import java.lang.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * TO-DO
+ */
+public class MyProgram {
+} // end of class MyProgram
